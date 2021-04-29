2087c4b15d9e696b00b2cb124838244eab4adba0
Brian aguiar <briantbini@outlook.com>
2021-04-29 14:24:20 -0300

    Tchau

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -9,6 +9,7 @@ namespace ProjetoTeste
             Console.WriteLine("Ola mundo!");
             Console.WriteLine("Como vc esta hoje ?");
             Console.WriteLine("Tenha um bom dia?");
+            Console.WriteLine("TChau");
             Console.ReadLine();
         }
     }
